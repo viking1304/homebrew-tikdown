659710ed820b19d840d4a50d26fae8859e02e795
Tairraos <xiaole.tao@gmail.com>
2022-08-19 13:04:46 +0800

    Upgrade release 1.2.0
    Support for new TikTok share URL Pre-release
    
    In this pre-release, there are 2 new changes:
    
    The filename will be created without any tags like #tiktok, #dance, etc., and the upload date would be added to the file name for reference.
    Supports the new https://vm.tiktok.com/xxxxx share links.

--- Casks/tikdown.rb
+++ Casks/tikdown.rb
@@ -1,6 +1,6 @@
 cask "tikdown" do
-  version "1.1.1"
-  sha256 "686e250cd7807ff778ab79e4df4a79fa4d7c13cf67585d52abbb4f33719ae0f8"
+  version "1.2.0"
+  sha256 "9f8f06ea54dc628b9356798f55ec311d1b9ef3a57ef53674d7b03230ad6f5e81"
 
   url "https://github.com/Tairraos/TikDown/releases/download/v#{version}/Tikdown-#{version}.dmg"
   name "TikDown"
