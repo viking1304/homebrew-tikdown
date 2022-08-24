aac59efcddf07a2dad0cfce0bb6508360ab5a54f
Tairraos <xiaole.tao@gmail.com>
2022-08-24 16:20:49 +0800

    Update tikdown.rb

--- Casks/tikdown.rb
+++ Casks/tikdown.rb
@@ -1,6 +1,6 @@
 cask "tikdown" do
-  version "1.2.0"
-  sha256 "9f8f06ea54dc628b9356798f55ec311d1b9ef3a57ef53674d7b03230ad6f5e81"
+  version "1.2.1"
+  sha256 "74aed53718bc3039ac969d3f4c05ad30b5432c98d3f3f6843ebe1449577b682e"
 
   url "https://github.com/Tairraos/TikDown/releases/download/v#{version}/Tikdown-#{version}.dmg"
   name "TikDown"
