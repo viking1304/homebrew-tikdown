fc7f55fe35ead1d4bbce34a97d6cbabb56ac3086
viking1304 <viking1304@gmail.com>
2022-12-22 11:17:42 +0100

    Style and live-check fix

--- Casks/tikdown.rb
+++ Casks/tikdown.rb
@@ -1,23 +1,17 @@
 cask "tikdown" do
-  version "1.1.1"
-  sha256 "686e250cd7807ff778ab79e4df4a79fa4d7c13cf67585d52abbb4f33719ae0f8"
+  version "1.2.2"
+  sha256 "eca5a2154e4a93b44a1b18e900f837ecfc812494bb4ef7cfd416dbdc909b425e"
 
   url "https://github.com/Tairraos/TikDown/releases/download/v#{version}/Tikdown-#{version}.dmg"
   name "TikDown"
   desc "Tiktok/Douyin video downloader"
   homepage "https://github.com/Tairraos/TikDown/"
 
-  livecheck do
-    url :stable
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
-  end
-
   app "Tikdown.app"
 
   zap trash: [
     "~/Library/Application Support/Tiktok Downloader",
     "~/Library/Preferences/com.tairraos.tikdown.plist",
-    "~/Library/Saved Application State/com.tairraos.tikdown.savedState"
+    "~/Library/Saved Application State/com.tairraos.tikdown.savedState",
   ]
-
 end
